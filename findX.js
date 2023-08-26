5e65260c056befa850c9bcfc79769f8eea04db8e
satyamsb1 <satyamborade45@gmail.com>
2023-08-26 19:35:24 +0530

    Added the mathematical operators code

---
+++ findX.js
@@ -0,0 +1,6 @@
+function findX(X) {
+    // Write code here
+    X*=3;
+    X+=10;
+    console.log(X);
+}
---
+++ sumOfFive.js
@@ -0,0 +1,4 @@
+function sumOfFive(one, two, three, four, five) {
+    console.log(one+two+three+four+five)
+ }
+ 
