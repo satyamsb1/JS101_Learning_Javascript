234b59cc6da3cb3232994d558642bf07a9733c93
satyamsb1 <satyamborade45@gmail.com>
2023-08-26 19:11:48 +0530

    first commit

---
+++ Intro/problem3.js
@@ -0,0 +1,17 @@
+/* Create a variable name to store your name
+Create a variable age to store your age
+Print the values stored in the variable on one line followed by the type of the variable in the next line */
+
+
+// Assigning variables 
+var name = "Satyam";
+var age = 21;
+
+// Printing the Name and age
+
+console.log("My name is " + name + ", I am " + age + " years old.");
+
+
+// Types of variables
+console.log(typeof name);
+console.log(typeof age);
---
+++ Intro/problem4.js
@@ -0,0 +1,34 @@
+/* Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+
+Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration */
+
+
+// Variables declaration
+var name = "Satyam";
+var school = "Sacred Heart School, Nashik";
+var grade = "10th";
+var section = "A";
+var rollNo = 26;
+var mathsMarks = 89;
+var sciMarks = 90;
+var engMarks = 82;
+
+
+// Used some keyboard symbols to create a report card
+
+console.log(" ______________________________________");
+console.log("|              Report-card             |");
+console.log("|--------------------------------------|");
+console.log("|Name:             "+name+"              |")
+console.log("|School:  " + school + "  |");
+console.log("|Grade:             " + grade + "               |");
+console.log("|Section:            " + section + "                 |");
+console.log("|Roll no:            " + rollNo + "                |");
+console.log("|--------------------------------------|");
+console.log("|Subjects           Marks              |");
+console.log("|Maths:              "+ mathsMarks +"                |");
+console.log("|Science:            "+ sciMarks +"                |");
+console.log("|English:            "+ engMarks +"                |");
+console.log("|______________________________________|");
